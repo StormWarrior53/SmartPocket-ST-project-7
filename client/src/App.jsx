54aa840a001322d2e73a0165efd40cc2d84e2d84
Plamen Draganov <plamine95@gmail.com>
2025-11-25 19:34:55 +0200

    Create About.jsx
    Create an About component with information about this finance instrument

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router"
+import About from "./components/about-page/About.jsx"
+
 function App() {
 
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <>
+        <header></header>
+
+        <Routes>
+            <Route path="/about" element={<About />} />
+        </Routes>
+        <footer></footer>
+        </>
     )
 }
 
